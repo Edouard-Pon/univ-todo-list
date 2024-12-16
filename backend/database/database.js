4d97424b9158877323031c82d9311727de0d69de
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-12-16 15:02:54 +0100

    Initial setup

---
+++ backend/database/database.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(process.env.DATABASE_URL, { useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db(process.env.DATABASE_NAME);
+  } catch (err) {
+    console.error('Database connection failed', err);
+    process.exit(1);
+  }
+}
+
+module.exports = { connectToDatabase };
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+﻿if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+const userRouter = require('./routes/user')
+
+app.use('/user', userRouter)
+
+app.listen(process.env.PORT || 3000)
