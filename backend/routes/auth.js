bcd37b126e8b01f8bd7c2d8fae8db5073f0441f1
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-12-16 17:16:51 +0100

    typo: user {user{user{user}}}

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -26,11 +26,11 @@ router.post('/login', async (req, res) => {
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-        const isMatch = await bcrypt.compare(password, user.mdp);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ error: 'Invalid credentials' });
         }
-        return res.status(200).json({ message: 'Logged in', user: { user } });
+        return res.status(200).json({ message: 'Logged in', user });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
